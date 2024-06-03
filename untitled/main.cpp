13c580c4528b314a6f3f0b48c8502e67a7630df7
Rostislav <rkashpurenko@gmail.com>
2024-06-03 19:55:59 +0300

    Practicing some commands

---
+++ untitled/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int chislo = 0;
+    int mnojitel = 0;
+
+    cout << "Vvedite chislo: ";
+    cin >> chislo;
+    cout << "Vvedite mhojitel: ";
+    cin >> mnojitel;
+    cout << "Result: " << chislo * mnojitel << endl;
+
+    system("pause");
+}
