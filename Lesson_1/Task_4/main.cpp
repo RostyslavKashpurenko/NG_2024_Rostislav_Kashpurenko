f58c2865f2ede8e6cce1885518a3602701a2c428
Rostislav <rkashpurenko@gmail.com>
2024-06-04 14:13:42 +0300

    Added Task 1

---
+++ Lesson_1/Task_4/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int Num0 = 0;
+    int Num1 = 0;
+    int operation;
+
+    cout << "Enter first number: ";
+    cin >> Num0;
+    cout << "Enter second number: ";
+    cin >> Num1;
+cout << "Enter operation: 1-'+', 2-'-', 3-'*', 4-/, 5-'pow', 6-'sqrt' (first number)\n";
+    cin >> operation;
+
+    switch(operation)
+    {
+    case 1:
+        cout << Num0 + Num1;
+        break;
+    case 2:
+        cout << Num0 - Num1;
+        break;
+    case 3:
+        cout << Num0 * Num1;
+        break;
+    case 4:
+        cout << Num0 / Num1;
+        break;
+    case 5:
+        cout << pow(Num0,Num1);
+        break;
+    case 6:
+        cout <<sqrt(Num0);
+        break;
+    }
+    return 0;
+}
