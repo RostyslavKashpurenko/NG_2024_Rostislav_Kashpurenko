5ff0ca82e81f920f8d9515d61088fa880b0b7f81
Rostislav <rkashpurenko@gmail.com>
2024-06-05 19:38:40 +0300

    Added Task 6

---
+++ Lesson_1/Task_6/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int height = 0;
+
+    cout << "Enter size: ";
+    cin >> height;
+
+    cout << endl;
+
+    for (int a = 1; a <= height; a++)
+    {
+        int stars = (a * 2) - 1;
+
+        int space = height - a + 1;
+
+        for (int b = 0; b < space; b++)
+            cout << " ";
+
+        for (int b = 0; b < stars; b++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    for (int a = 0; a < 1; a++)
+    {
+
+        for (int b = 0; b < height; b++)
+            cout << " ";
+
+
+        cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
