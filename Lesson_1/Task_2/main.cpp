c5c536b2fee9c00acf527e4d7049d44f9ff4669a
Rostislav <rkashpurenko@gmail.com>
2024-06-05 18:33:20 +0300

    Added Task 2

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Num0 = 0;
+    int Num1 = 0;
+
+    cout << "Enter first number: ";
+    cin >> Num0;
+    cout << endl;
+    cout << "Enter second number: ";
+    cin >> Num1;
+    cout << endl;
+
+    int Res = Num0 + Num1;
+
+    cout << "Result : " << Res << endl;
+    return 0;
+}
