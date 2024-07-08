fc778c2f33b5114d55e0db3b4ad1584d13a81fcd
Rostislav <rkashpurenko@gmail.com>
2024-07-08 08:20:51 +0300

    Edited Task 5

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -3,39 +3,24 @@
 
 using namespace std;
 
-int main()
-{
-    float Num0 = 0;
-    float Num1 = 0;
-    float Num2 = 0;
+int main() {
+    double a, b, c;
+    cout << "Enter coefficients a, b, and c: ";
+    cin >> a >> b >> c;
 
+    double discriminant = b * b - 4 * a * c;
 
-    cout << "Number 'a': ";
-    cin >> Num0;
-    cout << "Number 'b': ";
-    cin >> Num1;
-    cout << "Number 'c': ";
-    cin >> Num2;
+    if (discriminant >= 0) {
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Root 1: " << root1 << endl;
+        cout << "Root 2: " << root2 << endl;
+    } else {
+        double realPart = -b / (2 * a);
+        double imaginaryPart = sqrt(-discriminant) / (2 * a);
+        cout << "Root 1: " << realPart << " + " << imaginaryPart << "i" << endl;
+        cout << "Root 2: " << realPart << " - " << imaginaryPart << "i" << endl;
+    }
 
-    float Desc = pow(Num1, 2) - 4 * Num0 * (-1 * Num2);
-    float x1 = (-Num1 + sqrt(Desc)) / (2 * Num0);
-    float x2 = (-Num1 - sqrt(Desc)) / (2 * Num0);
-
-    if (Desc == 0 )
-        {
-            cout << "x1 , x2 = " << x1 << endl;
-
-            return 0;
-
-        }
-    else if (Desc > 0)
-        {
-            cout << "x1 = " << x1 << endl;
-            cout << "x2 = " << x2 << endl;
-
-            return 0;
-
-        }
-
-    cout << "No solution..." << endl;
+    return 0;
 }
